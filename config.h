7df773aa512ea225b759f4f9ad7d51748a24d564
valdeg <mailme@valdeg.xyz>
2020-09-24 09:49:11 +0300

    dmenu color fix

--- config.h
+++ config.h
@@ -70,7 +70,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_background, "-nf", col_gray, "-sb", col_violet, "-sf", col_foreground, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_background, "-nf", col_foreground, "-sb", col_red, "-sf", col_foreground, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
